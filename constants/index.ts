aae04a12826ff5638249b18890a62006e392762e
Affan Imran <affanimran1303@gmail.com>
2024-11-14 22:53:35 +0500

    Initial Commit:Navbar Setup

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+export const NAV_LINKS=[
+    {href:'/',key:"home",label:'Home'},
+    {href:'/',key:"how_voyagen_works",label:'How Voyagen Works?'},
+    {href:'/',key:"services",label:'Services'},
+    {href:'/',key:"pricing",label:'Pricing'},
+    {href:'/',key:"contact_us",label:'Contact Us'},
+];
+
+export const PEOPLE_URL=[
+    ''
+]
