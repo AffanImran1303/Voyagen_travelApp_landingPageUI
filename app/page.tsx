b96041748d847748396a8061b024da8061966da7
Affan Imran <affanimran1303@gmail.com>
2024-11-15 23:18:25 +0500

    Initialized other components and added navbar icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <Hero/>
+    <Features/>
+    <Guide/>
+    <GetApp/>
+    </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,54 @@
+"use client";
 import Link from "next/link";
-import Image from "next/image";
 import { NAV_LINKS } from "@/constants";
+import Button from "./Button";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen,setIsOpen]=useState(false);
+  const toggleMenu=():void=>{
+    setIsOpen(!isOpen);
+  }
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <p className="text-[#f25b08] font-bold text-2xl">Voyagen</p>
       </Link>
-        <ul className="hidden h-full gap-12 lg:flex">
+        <ul className='hidden h-full gap-12 lg:flex'>
           {NAV_LINKS.map((link)=>(
             <Link href={link.href} key={link.key}
             className="regular-16 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
               {link.label}
             </Link>
           ))}
-
         </ul>
-      
+        <div className="lg:flexCenter hidden">
+          <Button
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+          />
+        </div>
+        <button className="inline-block cursor-pointer lg:hidden" onClick={toggleMenu}>
+          {isOpen?(
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+
+          ):(
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+          )}
+        </button>
+        {/* {isOpen && (
+          <div className="lg:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 text-black sm:px-3">
+              <div className="bg-black"></div>
+            </div>
+
+          </div>
+        )} */}
     </nav>
   )
 }
 
 export default Navbar
+
