9fd32eec7a2e8506a6b098469e5871ff6981c2ba
Affan Imran <affanimran1303@gmail.com>
2024-11-16 19:51:59 +0500

    Updated Login Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,7 +3,7 @@ type ButtonProps={
     type:'button'|'submit';
     title:string;
     icon?:string;
-    variant:'btn_dark_green'
+    variant:'btn_primary'
 }
 
 import React from 'react'
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,11 +26,10 @@ const Navbar = () => {
           <Button
             type="button"
             title="Login"
-            icon="/user.svg"
-            variant="btn_dark_green"
+            variant="btn_primary"
           />
         </div>
-        <button className="inline-block cursor-pointer lg:hidden" onClick={toggleMenu}>
+        <button className="inline-block lg:hidden" onClick={toggleMenu}>
           {isOpen?(
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
 
